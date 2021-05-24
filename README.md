f8ccb54196075e90e215d4ae58719889e54f7892
Mingwei Deng <34043558+DDDennnisss@users.noreply.github.com>
2021-05-24 05:56:29 -0400

    typescript 封装

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 self-learning typescript Notes
 
-abstract class: 抽象类： 专门用来继承的，可以添加抽象方法，不能创建对象。
-abstract method():void; 抽象方法： 没有方法体，只能定义在抽象类中，子类必须对抽象方法进行重写。
+1. abstract class: 抽象类： 专门用来继承的，可以添加抽象方法，不能创建对象。
+   abstract method():void; 抽象方法： 没有方法体，只能定义在抽象类中，子类必须对抽象方法进行重写。
 
-interface :接口可以在定义类的时候区限制类的结构，用来定义一个类中包含哪些属性和方法，同时接口也可以当成类型声明去使用。
-就是对类的限制和规范
+2. interface :接口可以在定义类的时候区限制类的结构，用来定义一个类中包含哪些属性和方法，同时接口也可以当成类型声明去使用。
+   就是对类的限制和规范
 
 type myType = { //类型声明，不能重复声明
 name: string;
@@ -17,3 +17,7 @@ interface myInterface{ //接口声明，能重复声明， 如果重复声明，
 name: string;
 age: number;
 }
+
+3. 属性的封装: public
+   private: 只能在类里访问
+   ...
--- src/message.ts
+++ src/message.ts
@@ -1,1 +1,40 @@
-export const hi = "hello";
+(function(){
+  class Person {
+    // private _name: string;
+    // private _age: number;
+
+    // constructor(_name:string, _age:number){
+    //   this._name = _name;
+    //   this._age = _age;
+    // }
+
+    //等同于上面
+    constructor(private _name:string , private _age:number){
+
+    }
+
+    get name(){
+      return this._name
+    }
+
+    get age(){
+      return this._age;
+    }
+
+    set name(value:string){
+      this._name = value;
+    }
+
+    set age(value: number){
+      if(value>=0){
+        this._age = value;
+      }
+    }
+  }
+
+  const per = new Person("Dennis",20)
+  per.name;
+  per.age = 33;
+  console.log(per);
+  
+})();
